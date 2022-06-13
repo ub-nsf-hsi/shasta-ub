c843156136096877fa2d5caf40bdfc2820b8d667
Hemu <hemanthm@buffalo.edu>
2022-06-13 15:19:18 -0500

    Update states.py

--- experiments/complex_experiment/states.py
+++ experiments/complex_experiment/states.py
@@ -6,7 +6,7 @@ import numpy as np
 from sklearn.cluster import KMeans
 
 from .target import TargetManager
-import itertools
+
 
 def cluster(vehicles, n_clusters, config):
     """Performs k-means clustering on the given vehicle data.
@@ -185,7 +185,9 @@ def flatten_state_list(lists):
         A list contaning the states of all the groups
     """
     for el in lists:
-        if isinstance(el, collections.abc.Iterable) and not isinstance(el, (str, bytes)):
+        if isinstance(el, collections.abc.Iterable) and not isinstance(
+            el, (str, bytes)
+        ):
             yield from flatten_state_list(el)
         else:
             yield el
@@ -197,15 +199,12 @@ class StatesExtractor:
         self.config = config
         self.target_manager = TargetManager(config, core)
         self.map = core.get_map()
-        self.pareto_nodes  =None
+        self.pareto_nodes = None
 
     def calculate_distance(self, target, node):
         # Convert to cartesian
         cart_node = self.map.convert_to_cartesian([node['y'], node['x']])
         cart_target = self.map.convert_to_cartesian([target['y'], target['x']])
-
-        #edited by prajit 
-        
 
         # Calculate distance
         dist = np.linalg.norm(cart_target - cart_node)
@@ -224,17 +223,16 @@ class StatesExtractor:
             for k, target in enumerate(self.config['simulation']['target_building_id']):
                 target_info = self.target_manager.get_target_info(target)
 
-
                 node_info = self.map.get_node_info(j)
 
-                
-                dist = self.calculate_distance(self.map.get_node_info(target_info["id"]), node_info)
+                dist = self.calculate_distance(
+                    self.map.get_node_info(target_info["id"]), node_info
+                )
                 probability_goals = target_info['probability_goals']
 
                 # Calculate importance
-                importance[j, k] = probability_goals * 1 / (dist) + 0.01
+                importance[j, k] = probability_goals * 1 / (dist + 0.001)
         # Top five pareto nodes
-        
         pareto_nodes = pareto_opt(-1 * importance, n_nodes, n_targets, n_keep_in_pareto)
         return pareto_nodes
 
@@ -365,8 +363,7 @@ class StatesExtractor:
 
     def get_state(self, uav, ugv):
         """Get the state of the mission."""
-        
-        
+
         """
         1) Remaining time 1 - added
         2) Prob. of the Target to be the Actual Target 3 - added
@@ -376,14 +373,14 @@ class StatesExtractor:
 
         """
         state = []
+
         # Update the states with time
         remaining_time = self.config['simulation']['total_time'] - self.current_time
-        
-        normalized_time = (remaining_time - 0) / (self.config['simulation']['total_time']- 0)
-        
+        normalized_time = (remaining_time - 0) / (
+            self.config['simulation']['total_time'] - 0
+        )
         state.append([normalized_time])
 
-        
         # Perform clustering on UAV and UGV
         n_ugv_clusters = self.config['simulation']['n_ugv_clusters']
         n_uav_clusters = self.config['simulation']['n_uav_clusters']
@@ -392,11 +389,11 @@ class StatesExtractor:
         cluster_id_uav, uav_cluster_pos = cluster(uav, n_uav_clusters, self.config)
 
         # Perform pareto optimisation
-        pareto_nodes = self.get_pareto_nodes_online()
-        self.set_pareto_nodes(pareto_nodes)
+        self.pareto_nodes = self.get_pareto_nodes_online()
+
         # Get pareto node position
         pareto_node_pos = []
-        for node in pareto_nodes:
+        for node in self.pareto_nodes:
             node_info = self.map.get_node_info(node)
             position = self.map.convert_to_cartesian([node_info['y'], node_info['x']])
             pareto_node_pos.append(position[0:2])
@@ -407,49 +404,45 @@ class StatesExtractor:
         uav_group = self.get_group_info(
             cluster_id_uav, uav_cluster_pos, 'uav', pareto_node_pos
         )
-        
-        #calculate distance betwwen cluster and pareto node:
-        all_distance= list()
+
+        # Calculate distance between cluster and pareto node:
+        all_distance = list()
         for i in range(n_uav_clusters):
             for j in range(len(pareto_node_pos)):
-                distances= self.calculate_distance_pareto_vehicle(uav_cluster_pos[i], pareto_node_pos[j])
+                distances = self.calculate_distance_pareto_vehicle(
+                    uav_cluster_pos[i], pareto_node_pos[j]
+                )
                 all_distance.append(distances)
         for i in range(n_ugv_clusters):
             for j in range(len(pareto_node_pos)):
-                distances= self.calculate_distance_pareto_vehicle(ugv_cluster_pos[i], pareto_node_pos[j])
+                distances = self.calculate_distance_pareto_vehicle(
+                    ugv_cluster_pos[i], pareto_node_pos[j]
+                )
                 all_distance.append(distances)
-                
-        normalized_distance= (all_distance - np.min(all_distance)) / (np.max(all_distance) - np.min(all_distance)) 
-        # Consolidate the states to a long vector
-        state.append(normalized_distance)
-        
-        # for i in range(3):
-        #     state.append(uav_group[i]['state'])
-        # for i in range(3):
-        #     state.append(ugv_group[i]['state'])
 
+        normalized_distance = (all_distance - np.min(all_distance)) / (
+            np.max(all_distance) - np.min(all_distance)
+        )
+
+        # Add normalised distance
+        state.append(normalized_distance)
+
+        # Missing probability
+        for i in range(3):
+            state.append(self.target_manager.targets[i]["probability_goals"])
 
         # Convert everything into a list
         state = list(flatten_state_list(state))
-  
-        #missing probability
-        #adding below
-        for i in range(3):
-            state.append(self.target_manager.targets[i]["probability_goals"])
+
         return state
 
     def update_progrees(self, uav, ugv):
         done = self.target_manager.update_progress(uav, ugv)
         return done
-    
 
-    def calculate_distance_pareto_vehicle(self, pareto, vehicle):
-        dist = np.linalg.norm(pareto - vehicle)
+    def calculate_distance_pareto_vehicle(self, pareto_pos, vehicle_pos):
+        dist = np.linalg.norm(pareto_pos - vehicle_pos)
         return dist
     
-    def set_pareto_nodes(self,pareto_nodes):
-        self.pareto_nodes  =pareto_nodes
-        
     def get_pareto_node(self):
         return self.pareto_nodes
-        
