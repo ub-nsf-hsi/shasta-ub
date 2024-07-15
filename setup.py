734eb3d7166b2e71edd341c25d48595fbc1e43d6
Hemu <hemanthm2277@gmail.com>
2024-07-15 14:33:57 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
@@ -34,7 +31,7 @@ setup(
     description="SHaSTA:  A simulator for human and swarm team applications. ",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=history,
     include_package_data=True,
     keywords='shasta',
     name='shasta',
